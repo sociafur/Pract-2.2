8a13eacf4b654a80f9c56ae4f781d73799203d7f
asus <asus@DESKTOP-7KH53KG>
2022-02-12 20:11:36 +0300

    Добавьте файлы проекта.

---
+++ Pract 2.2/Pract 2.2.cpp
@@ -0,0 +1,37 @@
+﻿// Pract 2.2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+double x;
+double a;
+double Step(double x,double a)
+{
+    if(x > 8 )
+    {
+        return 1 / (pow(2, x + a));
+    }
+    if (5 < x && x < 8)
+    {
+        return tan(a * x);
+    }
+    else
+    {
+        pow(a, x);
+    }
+    
+}
+void StepAnalog(double x,double a)
+{
+    double result = Step(x, a);
+    std::cout << std::fixed << std::setprecision(4);
+    std::cout << result << std::endl;
+    std::cin.get();
+}
+int main()
+{
+    StepAnalog(6,2);
+}
+
+
